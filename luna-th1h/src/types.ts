be84427af79f06a970a52d7d73b842398045d3c2
sakdiphongkan-ops <sakdiphong_kan@hotmail.com>
2026-09-18 12:17:19 +0700

    LUNA-TH1H: add luna-th1h/src/types.ts

---
+++ luna-th1h/src/types.ts
@@ -0,0 +1,9 @@
+export type Side = "BUY" | "SELL";
+
+export interface Quote {
+  symbol:string; ts:string; bid:number|null; ask:number|null; last:number|null;
+  bidSize:number|null; askSize:number|null; source:string;
+}
+export interface Signal {
+  symbol:string; ts:string; action:"BUY"|"SELL"|"HOLD"; reason:string; strategyVersion:string;
+}
