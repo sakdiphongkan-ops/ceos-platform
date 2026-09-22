89e0997098ebadef4f730a585a4d9232726920e7
sakdiphongkan-ops <sakdiphong_kan@hotmail.com>
2026-09-22 07:19:16 +0700

    security(luna): pin trigger function search path

---
+++ luna-th1h/migrations/20260922070000_luna_security_search_path_v1.sql
@@ -0,0 +1,10 @@
+create or replace function public.luna_touch_updated_at()
+returns trigger
+language plpgsql
+set search_path = public
+as $function$
+begin
+  new.updated_at = now();
+  return new;
+end;
+$function$;
